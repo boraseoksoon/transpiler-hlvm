ac0ba3c624d09b014a32332f81605216139720ce
seoksoon <seoksun.jang@shopkick.com>
2021-08-02 13:18:15 -0400

    [ADD] functions

--- main.swift
+++ main.swift
@@ -9,15 +9,23 @@ import Foundation
 import SwiftSyntax
 
 let sourceCode = swiftSource
-let destinationLanguage = Language.python
 
-let AST = try SyntaxParser.parse(source: sourceCode.trim(for:destinationLanguage))
-let destinationCode = generate(from: AST, to:destinationLanguage)
+let destinationCode = transpile(from: sourceCode)
 
+print("destinationCode : \(destinationCode)")
 print("result >>")
 print(destinationCode)
 
 print("********************************************")
+
+func transpile(from source: String) -> String {
+    let destinationLanguage = Language.python
+    
+    let AST = try! SyntaxParser.parse(source: sourceCode.trim(for:destinationLanguage))
+    let destinationCode = generate(from: AST, to: destinationLanguage)
+    
+    return destinationCode
+}
 
 func generate(from AST: SourceFileSyntax, to language: Language) -> String {
     let transformedSyntax = Visitor(language: language).visit(AST)
